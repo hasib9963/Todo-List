094e6cae08be6dc6decccf7fc67c7df633176675
Hasib Mohammad Mahede Hasan <hasib9963@gmail.com>
2024-10-23 10:35:21 +0600

    implement add todo list item functionlity

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
     const addBtn = document.getElementById('add-btn');
-    const newTaskInput = document.getElementById('new-task');
+    const newTodoInput = document.getElementById('new-todo');
     const todoList = document.getElementById('todo-list');
+
+    addBtn.addEventListener('click', addTodo);
+
+    function addTodo() {
+        const todoText = newTodoInput.value.trim();
+
+        if (todoText === '') {
+            alert('Please enter text.');
+            return;
+        }
+
+        const todoItem = document.createElement('li');
+        todoItem.classList.add('flex', 'items-center', 'justify-between', 'bg-gray-100', 'p-3', 'rounded-md', 'shadow-sm');
+    }
 
 });
