cc49ce3315fdc6897176b6f52da796d34437cb6f
Hasib Mohammad Mahede Hasan <hasib9963@gmail.com>
2024-10-23 10:20:19 +0600

    Implement To-Do List functionality using EventListner

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const addBtn = document.getElementById('add-btn');
+    const newTaskInput = document.getElementById('new-task');
+    const todoList = document.getElementById('todo-list');
+
+});
